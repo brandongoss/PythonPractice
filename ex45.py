322d24e8c1f01847591b4e4474f91b5f8d53c876
Brandon Goss <bgoss@BrandonsMacBook.home>
2017-05-15 18:54:03 -0400

    Keep getting the TypeError: BrandonsMacBook:PythonPractice bgoss$ python ex45.py
            Wow! It's really dark in here.
            As you slowly walk through the maze, you notice something.
            A door! Will you enter or pass this door?
    
    > enter
    Traceback (most recent call last):
      File "ex45.py", line 183, in <module>
        a_game.play()
      File "ex45.py", line 27, in play
        next_scene_name = current_scene.enter()
    TypeError: enter() takes exactly 2 arguments (1 given)
    
    I feel like in the final lines where I do the calls i need to somehow
    pass an argument to include the weapons but I'm not sure how. Been stuck
    on this program for a few weeks now.

--- ex45.py
+++ ex45.py
@@ -1,18 +1,33 @@
-import dice
 from sys import exit
+
+from random import randint
 
 class Scene(object):
 # nothing to put in superclass, just practicing inheritance
     def enter(self):
         pass
 
+class Player(object):
+
+    weaponlist = []
+
+    def set_weapon(self, selection):
+        weaponlist.append(selection)
+
 class Engine(object):
 
     def __init__(self, scene_map):
-        pass
+        self.scene_map = scene_map
 
-    def play(self):
-        pass
+    def play(self, Player):
+        current_scene = self.scene_map.opening_scene()
+        last_scene = self.scene_map.next_scene('Finish')
+
+        while current_scene != last_scene:
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_name)
+
+        current_scene.enter()
 
 class Death(Scene):
 
@@ -24,8 +39,7 @@ class Death(Scene):
 
     def enter(self):
 
-        selection = dice.roll('1d3')
-        print Death.messages(selection)
+        print Death.messages[randint(0, len(messages)-1)]
         exit(1)
 
 class Atrium(Scene):
@@ -40,7 +54,7 @@ class Atrium(Scene):
 
         selection = raw_input("> ")
 
-        if selection == 'enter'
+        if selection == 'enter':
             return 'FireRoom'
 
         else:
@@ -49,7 +63,7 @@ class Atrium(Scene):
 
 class FireRoom(Scene):
 
-    def enter(self):
+    def enter(self, Player):
         print """
         A great blaze appears on either side of you.
         As you walk through the fire laden corridor, you notice something.
@@ -62,17 +76,19 @@ class FireRoom(Scene):
         selection = raw_input("> ")
 
         if selection == 'pocket watch':
+            Player.set_weapon(selection)
             return 'CerberusRoom'
 
         elif selection == 'torch':
-            return 'Cavern'
+            Player.set_weapon(selection)
+            return 'CerberusRoom'
 
         else:
             return 'Death'
 
 class CerberusRoom(Scene):
 
-    def enter(self):
+    def enter(self, Player):
         print """
         As the door swings open you notice a great three headed beast before you.
         Awoken by stone door hitting the opposing wall, up stands the massive
@@ -87,17 +103,53 @@ class CerberusRoom(Scene):
             return 'Death'
 
         elif selection == 'fight':
-            return
+            for name in weaponlist:
+                if name == 'torch':
+                    print """
+                    The Cerberus, fearing the fire, hides in the corner.
+                    You walk by him and proceed through the next door.
+                    """
+                    return 'MinotaurRoom'
 
-class Cavern(Scene):
+                if name == 'pocket watch':
+                    print """
+                    Slowly you start to swing the watch back and forth.
+                    The Cerberus becomes entranced, and eventually hypnotized.
+                    Under the spell of your command, he his now at your service.
+                    You and the Cerberus walk into the next room together.
+                    """
+                    Player.set_weapon('Cerberus')
+                    return 'MinotaurRoom'
 
-    def enter(self):
-        pass
+                else:
+                    return 'Death'
 
 class MinotaurRoom(Scene):
 
-    def enter(self):
-        pass
+    def enter(self, Player):
+        print """
+        Icarus! There he is! But a massive beast stands before you, with
+        the body of a man and the head of a bull. You cannot outrun him,
+        you must fight him if you wish to save your dear friend.
+        """
+
+        for name in weaponlist:
+            if name == 'torch':
+                print "The Minotaur is unafraid of fire. This is not good."
+                return 'Death'
+
+            if name == 'Cerberus':
+                print """
+                Good thing you have the Cerberus with you, he will do the fighting.
+                Back and forth the two battle, until eventually the Cerberus bites
+                the Minotaur's head off. Meanwhile you sit back and watch on as
+                your hypontized minion does all the work.
+                """
+                return 'Finish'
+
+            else:
+                print "You have no weapons! Oh no!"
+                return 'Death'
 
 class Finish(Scene):
 
@@ -105,52 +157,27 @@ class Finish(Scene):
         print "You win! Icarus is saved and the Minotaur is dead!"
         return 'Finish'
 
-class Weapon(object):
-    
-    def pick_up(self):
-        pass
-
-class Torch(Weapon):
-
-    def throw(self):
-        pass
-
-class PocketWatch(Weapon):
-
-    def hypnotize(self):
-        pass
-
-class Cerberus(Weapon):
-
-    def eat(self):
-        pass
-
 class Map(object):
 
     scenes = {
         'Atrium': Atrium(),
         'FireRoom': FireRoom(),
         'CerberusRoom': CerberusRoom(),
-        'Cavern': Cavern(),
         'MinotaurRoom': MinotaurRoom(),
         'Death': Death(),
         'Finish': Finish(),
     }
 
-    weapons = {
-        'torch': Torch(),
-        'pocket watch': PocketWatch(),
-        'cerberus': Cerberus(),
-    }
-
     def __init__(self, start_scene):
-        pass
+        self.start_scene = start_scene
 
-    def next_scene(self, current_scene):
-        pass
+    def next_scene(self, scene_name):
+        val = Map.scenes.get(scene_name)
+        return val
 
     def opening_scene(self):
-        pass
+        return self.next_scene(self.start_scene)
+
 
 a_map = Map('Atrium')
 a_game = Engine(a_map)
